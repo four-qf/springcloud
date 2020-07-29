55651913f3948afa5a076d714d690f71ee55ce30
qiuxian <qiuxian@sdbattery.com>
2020-07-29 23:19:50 +0800

    a

---
+++ cloud-gateway/src/main/java/com/qx/gateway/Test.java
@@ -0,0 +1,11 @@
+package com.qx.gateway;
+
+/**
+ * @author qiuxian
+ * @date 2020/7/29
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("a");
+    }
+}
