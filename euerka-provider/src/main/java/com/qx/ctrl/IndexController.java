f5966e7cd17450bac6f3a351bcc04dd98292cd76
qiuxian <qiuxian@sdbattery.com>
2021-04-22 09:09:52 +0800

    redis-cluster

--- euerka-provider/src/main/java/com/qx/ctrl/IndexController.java
+++ euerka-provider/src/main/java/com/qx/ctrl/IndexController.java
@@ -1,13 +1,24 @@
 package com.qx.ctrl;
 
+import com.qx.cache.RedisServcie;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
+    @Autowired
+    private RedisServcie redisServcie;
+
     @RequestMapping("/home")
     public String home() {
         return "hello world1";
     }
+
+    @RequestMapping("/queryCache")
+    public String queryCache(String key) {
+        return redisServcie.get(key);
+    }
+
 }
